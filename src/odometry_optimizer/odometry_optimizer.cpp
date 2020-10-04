10406ae759ec335c4e8e18373f4868e8addcd60b
sigtot <sigurdtotland@gmail.com>
2020-10-04 12:51:42 +0200

    Update topics in odometry optimizer to those echoed by the echo node

--- src/odometry_optimizer/odometry_optimizer.cpp
+++ src/odometry_optimizer/odometry_optimizer.cpp
@@ -11,9 +11,9 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     auto pub = nh.advertise<nav_msgs::Odometry>("/optimized_pose", 1000);
     ISAMOptimizer isamOptimizer(&pub, 3);
-    ros::Subscriber subImu = nh.subscribe("/rovio/odometry", 1000,
+    ros::Subscriber subImu = nh.subscribe("/echo/rovio/odometry", 1000,
                                           &ISAMOptimizer::recvRovioOdometryAndPublishUpdatedPoses, &isamOptimizer);
-    ros::Subscriber subLidar = nh.subscribe("/aft_mapped_to_init_CORRECTED", 1000,
+    ros::Subscriber subLidar = nh.subscribe("/echo/aft_mapped_to_init_CORRECTED", 1000,
                                             &ISAMOptimizer::recvLidarOdometryAndPublishUpdatedPoses, &isamOptimizer);
     ros::spin();
 }
