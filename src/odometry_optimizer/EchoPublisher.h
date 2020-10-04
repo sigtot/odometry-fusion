3b70b35f4ce531c3378a26271fe2cfdf31fec0a8
sigtot <sigurdtotland@gmail.com>
2020-10-04 12:45:04 +0200

    Make echo publisher generic in message type

--- src/odometry_optimizer/EchoPublisher.h
+++ src/odometry_optimizer/EchoPublisher.h
@@ -2,22 +2,34 @@
 #define SIMPLE_ODOMETRY_OPT_ROS_ECHOPUBLISHER_H
 
 #include <ros/ros.h>
+#include <iostream>
 #include <string>
-#include <nav_msgs/Odometry.h>
 
 using namespace std;
 
+template<class T>
 class EchoPublisher {
 private:
     string topic;
     ros::Publisher pub;
     ros::Subscriber sub;
 
-    void echo(const nav_msgs::Odometry &msg);
+    void echo(const T &msg);
 
 public:
-    EchoPublisher(const string& topic, ros::NodeHandle &nh);
+    EchoPublisher(const string &topic, ros::NodeHandle &nh);
 };
 
+template<class T>
+EchoPublisher<T>::EchoPublisher(const string &topic, ros::NodeHandle &nh) : topic(topic) {
+    pub = nh.advertise<T>("/echo/" + topic, 1000);
+    sub = nh.subscribe(topic, 1000, &EchoPublisher::echo, this);
+}
+
+template<class T>
+void EchoPublisher<T>::echo(const T &msg) {
+    pub.publish(msg);
+    cout << "Echoed msg on " << topic << endl;
+}
 
 #endif //SIMPLE_ODOMETRY_OPT_ROS_ECHOPUBLISHER_H
--- src/odometry_optimizer/echo_node.cpp
+++ src/odometry_optimizer/echo_node.cpp
@@ -1,9 +1,11 @@
 #include <ros/ros.h>
 #include "EchoPublisher.h"
 
+#include <nav_msgs/Odometry.h>
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "echo_node");
     ros::NodeHandle nh;
-    EchoPublisher rovioEchoPublisher("/rovio/odometry", nh);
+    EchoPublisher<nav_msgs::Odometry> rovioEchoPublisher("/rovio/odometry", nh);
     ros::spin();
 }
