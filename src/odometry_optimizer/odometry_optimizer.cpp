842d00afbb5d6ec37e147fbf459c8864e7e01105
sigtot <sigurdtotland@gmail.com>
2020-11-08 17:27:45 +0100

    Set topic names through ros launch parameters

--- src/odometry_optimizer/odometry_optimizer.cpp
+++ src/odometry_optimizer/odometry_optimizer.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include "ISAMOptimizer.h"
+#include <iostream>
 
 using namespace std;
 using namespace gtsam;
@@ -11,10 +12,24 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     auto pub = nh.advertise<nav_msgs::Odometry>("/optimized_pose", 1000);
     ISAMOptimizer isamOptimizer(&pub);
-    ros::Subscriber subIMU = nh.subscribe("/vn100/imu", 1000, &ISAMOptimizer::recvIMUMsgAndUpdateState, &isamOptimizer);
-    ros::Subscriber subRovio = nh.subscribe("/echo/rovio/odometry", 1000,
-                                            &ISAMOptimizer::recvRovioOdometryMsgAndPublishUpdatedPoses, &isamOptimizer);
-    ros::Subscriber subLidar = nh.subscribe("/echo/aft_mapped_to_init_CORRECTED", 1000,
-                                            &ISAMOptimizer::recvLidarOdometryMsgAndPublishUpdatedPoses, &isamOptimizer);
+    std::string imuTopicName, odometry1TopicName, odometry2TopicName;
+
+    if (!nh.getParam("imu_topic_name", imuTopicName)) {
+        cout << "Please supply a value for imu_topic_name" << endl;
+    }
+    nh.getParam("odometry1_topic_name", odometry1TopicName);
+    nh.getParam("odometry2_topic_name", odometry2TopicName);
+    ros::Subscriber subIMU = nh.subscribe(imuTopicName,
+                                          1000,
+                                          &ISAMOptimizer::recvIMUMsgAndUpdateState,
+                                          &isamOptimizer);
+    ros::Subscriber subRovio = nh.subscribe(odometry1TopicName,
+                                            1000,
+                                            &ISAMOptimizer::recvRovioOdometryMsgAndPublishUpdatedPoses,
+                                            &isamOptimizer);
+    ros::Subscriber subLidar = nh.subscribe(odometry2TopicName,
+                                            1000,
+                                            &ISAMOptimizer::recvLidarOdometryMsgAndPublishUpdatedPoses,
+                                            &isamOptimizer);
     ros::spin();
 }
